48a3e7993d2ff8d4b5e9e536f14e98aec9f03097
Guilherme S <guinho.shuto@gmail.com>
2023-01-18 20:53:30 -0400

    umas estilizacoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+
 function App() {
   const [temp, setTemp] = useState<number>(0)
+
   useEffect(() => {
     axios.get('https://api.open-meteo.com/v1/forecast?latitude=-20.45&longitude=-54.60&current_weather=true')
     .then((r: any) => r.data)
@@ -13,7 +14,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{temp}°</h1>
+      <div>
+        <h4>Campo Grande</h4>      
+        <h1>{temp}°</h1>
+      </div>
     </div>
   );
 }
